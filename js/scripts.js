bdda676feaed7662fc414c97b7cdf87e24061802
jacob ruleaux and max scher <max@>
2017-05-31 15:54:49 -0700

    add js and button function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function()) {
+  $("#form-one").submit(function(event) {
+    var repeatInput = $("input#repeat").val() ;
+
+    $(".repeat").text(repeatInput);
+
+    $("#shout").show();
+    event.preventDefault();
+  });
+}
